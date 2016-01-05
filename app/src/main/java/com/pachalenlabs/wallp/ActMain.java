d7b44b9abdae872cd0f7f8f17aa36de4dc249155
taeik e <tw2614@gmail.com>
2016-01-06 00:58:55 +0900

    test1

--- app/src/main/java/com/pachalenlabs/wallp/ActMain.java
+++ app/src/main/java/com/pachalenlabs/wallp/ActMain.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ActMain extends AppCompatActivity {
-
+//안녕하세요
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
