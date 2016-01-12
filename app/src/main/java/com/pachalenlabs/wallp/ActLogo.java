c7a60ee7c8f785f7cb48d230ebc9b774637dfca4
KimTaeIk <tw2614@gmail.com>
2016-01-13 01:01:56 +0900

    WP-13

--- app/src/main/java/com/pachalenlabs/wallp/ActLogo.java
+++ app/src/main/java/com/pachalenlabs/wallp/ActLogo.java
@@ -8,25 +8,20 @@ import android.view.animation.Animation;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Fullscreen;
-import org.androidannotations.annotations.res.AnimationRes;
-
-
 @Fullscreen
 @EActivity(R.layout.activity_act_logo)
 public class ActLogo extends Activity {
-
-    Handler delayTime;
-    @AnimationRes Animation fadeOut;
-    @AnimationRes Animation fadeIn;
-
+    Handler delayTime_;
     @AfterViews
     void setDelay(){
-        delayTime = new Handler();
-        delayTime.postDelayed(mrun, 2000); // 딜레이 ( 런어블 객체는 mrun, 시간 2초)
+        delayTime_ = new Handler();
+        delayTime_.postDelayed(mrun, 2000); // 딜레이 ( 런어블 객체는 mrun, 시간 2초)
     }
     Runnable mrun = new Runnable(){
         @Override
         public void run() {
+            Animation fadeOut;
+            Animation fadeIn;
             ActMain_.intent(getApplicationContext()).flags(Intent.FLAG_ACTIVITY_NEW_TASK).start();
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
