36630aeb9aa0d44799fd8be8680ef8dfac19db18
Niklane <cswcsy@gmail.com>
2016-01-15 00:22:11 +0900

    WP-9 Implemented - Base Layout(+Fragment)

---
+++ app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
@@ -0,0 +1,15 @@
+package com.pachalenlabs.wallp.ui.fragment;
+
+import android.app.Fragment;
+
+import com.pachalenlabs.wallp.R;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * Created by Niklane on 2016-01-15.
+ */
+
+@EFragment(R.layout.fragment_wallpaper)
+public class WallpaperFragment extends Fragment{
+}
