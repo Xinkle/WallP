6f090580516aaabc0bb0bb432c33a7d6a1c9ca42
KimTaeIk <tw2614@gmail.com>
2016-01-15 16:52:57 +0900

    WP-11   -요청하신 수정사항들 수정했습니다 글씨체 크기는 아직 만지지 않았습니다

--- app/src/main/java/com/pachalenlabs/wallp/ui/activity/MainActivity.java
+++ app/src/main/java/com/pachalenlabs/wallp/ui/activity/MainActivity.java
@@ -3,11 +3,28 @@ package com.pachalenlabs.wallp.ui.activity;
 import android.support.v7.app.AppCompatActivity;
 
 import com.pachalenlabs.wallp.R;
+import com.pachalenlabs.wallp.ui.fragment.InformationFragment;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.FragmentById;
 
 @EActivity(R.layout.activity_act_main)
 public class MainActivity extends AppCompatActivity {
 
-    
+    @FragmentById
+    InformationFragment PictureInformationFragment;
+    @FragmentById
+    InformationFragment ExchangeRatioFragment;
+
+    @AfterViews
+    public void setUp_UI(){
+        PictureInformationFragment = (InformationFragment) getFragmentManager().findFragmentById(R.id.PictureInformationFragment);
+        PictureInformationFragment.setTitle("사진수");
+        PictureInformationFragment.setValues(20);
+
+        ExchangeRatioFragment = (InformationFragment) getFragmentManager().findFragmentById(R.id.ExchangeRatioFragment);
+        ExchangeRatioFragment.setTitle("교체 주기");
+        ExchangeRatioFragment.setValues(30);
+    }
 }
