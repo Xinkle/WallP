023ff368ce5764a5a0d6366d5c1453f59d22bed2
KimTaeIk <tw2614@gmail.com>
2016-01-15 15:54:29 +0900

    WP-11   -Fragment 재활용 가능하다고 생각하고 수정 ㅇㅅㅇ;;

--- app/src/main/java/com/pachalenlabs/wallp/ui/fragment/InformationFragment.java
+++ app/src/main/java/com/pachalenlabs/wallp/ui/fragment/InformationFragment.java
@@ -1,13 +1,11 @@
 package com.pachalenlabs.wallp.ui.fragment;
 
 import android.app.Fragment;
-import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.pachalenlabs.wallp.R;
 
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 
@@ -21,12 +19,9 @@ public class InformationFragment extends Fragment{
     TextView textView1_,textView2_;
     @ViewById
     ImageButton informationIcon;
-    @AfterViews
-    public void setText(){
-        Bundle extra = getArguments();
-        String title = extra.getString("title");
+
+    public void setText(String title,int value){
         textView1_.setText(title);
-        if( title == "사진 수" ) textView2_.setText(extra.getInt("Pictures"));
-        else textView2_.setText(extra.getInt("exchangingPeriods"));
+        textView2_.setText(value);
     }
 }
