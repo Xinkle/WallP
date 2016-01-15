baf3f8e7b70b5143024375678aee243347095ecf
KimTaeIk <tw2614@gmail.com>
2016-01-15 09:31:54 +0900

    WP-11      info_layout 틀 구성

--- app/src/main/java/com/pachalenlabs/wallp/ui/fragment/InformationFragment.java
+++ app/src/main/java/com/pachalenlabs/wallp/ui/fragment/InformationFragment.java
@@ -1,16 +1,30 @@
 package com.pachalenlabs.wallp.ui.fragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.widget.TextView;
 
 import com.pachalenlabs.wallp.R;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 /**
- * Created by Niklane on 2016-01-15.
+ * Created by Taeike on 2016-01-15.
  */
 
 @EFragment(R.layout.fragment_information)
 public class InformationFragment extends Fragment{
 
+    @ViewById
+    TextView textView1_;
+    @ViewById
+    TextView textView2_;
+    @AfterViews
+    public void setText(){
+        Bundle extra = getArguments();
+        String id = extra.getString("title");
+        textView1_.setText(id);
+    }
 }
