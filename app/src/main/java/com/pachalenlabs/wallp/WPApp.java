38f645e4198b86608f4199beb4590287bdd63bdb
Niklane <cswcsy@gmail.com>
2016-01-17 21:03:17 +0900

    implemented below feature - Sample Toolbar - Grade version up to alpha 0.5

--- app/src/main/java/com/pachalenlabs/wallp/WPApp.java
+++ app/src/main/java/com/pachalenlabs/wallp/WPApp.java
@@ -8,7 +8,6 @@ import org.androidannotations.annotations.EApplication;
  * Created by Niklane on 2016-01-12.
  */
 
-@EApplication
 public class WPApp extends Application {
 
 }
--- app/src/main/java/com/pachalenlabs/wallp/ui/activity/MainActivity.java
+++ app/src/main/java/com/pachalenlabs/wallp/ui/activity/MainActivity.java
@@ -1,7 +1,15 @@
 package com.pachalenlabs.wallp.ui.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -12,19 +20,33 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.FragmentById;
 
-@EActivity(R.layout.activity_act_main)
+@EActivity
 public class MainActivity extends AppCompatActivity {
 
     @FragmentById
     InformationFragment PictureInformationFragment;
     @FragmentById
     InformationFragment ExchangeRatioFragment;
-    ImageButton PictureInformationFragment_InfoButton;
-    ImageButton ExchangeRatioFragment_InfoButton;
 
     String Tag = "MainActivity";
-    @AfterViews
-    public void setUp_UI(){
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
         PictureInformationFragment = (InformationFragment) getFragmentManager().findFragmentById(R.id.PictureInformationFragment);
         PictureInformationFragment.setTitle("사진수");
         PictureInformationFragment.setValues(20);
@@ -44,5 +66,27 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(Tag, "222222222222222222222222222");
             }};
         ExchangeRatioFragment.setInformationButtonClick(ExchangeOnInformationButtonClick);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
