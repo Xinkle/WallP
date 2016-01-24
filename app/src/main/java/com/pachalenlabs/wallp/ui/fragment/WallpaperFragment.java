2575d7005be0b42088f1017a0cf96602fd5f9f3a
KimTaeIk <tw2614@gmail.com>
2016-01-24 17:51:13 +0900

    WP-17  "wallpaperFragment 초안"

--- app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
+++ app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
@@ -1,10 +1,14 @@
 package com.pachalenlabs.wallp.ui.fragment;
 
 import android.app.Fragment;
+import android.util.Log;
+import android.widget.ImageButton;
 
 import com.pachalenlabs.wallp.R;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by Niklane on 2016-01-15.
@@ -12,4 +16,28 @@ import org.androidannotations.annotations.EFragment;
 
 @EFragment(R.layout.wallpaper_fragment)
 public class WallpaperFragment extends Fragment{
+
+    String Tag = "WallpaperFragment";
+
+    @ViewById(R.id.emptyPlace_imageButton)
+    ImageButton _emptyPlaceImageButton;
+    @ViewById(R.id.leftView_imageButton)
+    ImageButton _leftViewImageButton;
+    @ViewById(R.id.cancel_imageButton)
+    ImageButton _cancelImageButton;
+    @ViewById(R.id.rightView_imageButton)
+    ImageButton _rightViewImageButton;
+
+    @Click(R.id.leftView_imageButton)
+    void leftViewClicked(){
+        Log.d(Tag,"1");
+    }
+    @Click(R.id.cancel_imageButton)
+    void cancelClicked(){
+        Log.d(Tag,"2");
+    }
+    @Click(R.id.rightView_imageButton)
+    void rightViewClicked(){
+        Log.d(Tag,"3");
+    }
 }
