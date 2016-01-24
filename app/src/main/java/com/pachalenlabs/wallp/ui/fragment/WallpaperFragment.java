5c289ee890e8bafd9a55eb4042e3ea0bfa907731
Niklane <cswcsy@gmail.com>
2016-01-24 18:47:09 +0900

    Revert "WP-17  "wallpaperFragment 초안""
    This reverts commit 2575d7005be0b42088f1017a0cf96602fd5f9f3a.

--- app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
+++ app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
@@ -1,14 +1,10 @@
 package com.pachalenlabs.wallp.ui.fragment;
 
 import android.app.Fragment;
-import android.util.Log;
-import android.widget.ImageButton;
 
 import com.pachalenlabs.wallp.R;
 
-import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by Niklane on 2016-01-15.
@@ -16,28 +12,4 @@ import org.androidannotations.annotations.ViewById;
 
 @EFragment(R.layout.wallpaper_fragment)
 public class WallpaperFragment extends Fragment{
-
-    String Tag = "WallpaperFragment";
-
-    @ViewById(R.id.emptyPlace_imageButton)
-    ImageButton _emptyPlaceImageButton;
-    @ViewById(R.id.leftView_imageButton)
-    ImageButton _leftViewImageButton;
-    @ViewById(R.id.cancel_imageButton)
-    ImageButton _cancelImageButton;
-    @ViewById(R.id.rightView_imageButton)
-    ImageButton _rightViewImageButton;
-
-    @Click(R.id.leftView_imageButton)
-    void leftViewClicked(){
-        Log.d(Tag,"1");
-    }
-    @Click(R.id.cancel_imageButton)
-    void cancelClicked(){
-        Log.d(Tag,"2");
-    }
-    @Click(R.id.rightView_imageButton)
-    void rightViewClicked(){
-        Log.d(Tag,"3");
-    }
 }
