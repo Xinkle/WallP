eb38f9b616b418fd967f4136ae125781737bcb6e
Niklane <cswcsy@gmail.com>
2016-01-27 00:17:22 +0900

    WP-15 log4j for android implemented
    안드로이드 로그위한
    로그 라이브러리 적용

--- app/src/main/java/com/pachalenlabs/wallp/WPApp.java
+++ app/src/main/java/com/pachalenlabs/wallp/WPApp.java
@@ -1,13 +1,33 @@
 package com.pachalenlabs.wallp;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.pachalenlabs.wallp.module.WPLogger;
 
 import org.androidannotations.annotations.EApplication;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Niklane on 2016-01-12.
  */
 
+@EApplication
 public class WPApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setLogger();
+    }
 
+    void setLogger(){
+        try {
+            WPLogger.configure(getApplicationContext());
+
+            Logger logger = Logger.getLogger(WPApp_.class);
+            logger.info("Initialize Logger");
+        } catch (Exception e) {
+            Log.e("android-log4j", e.getMessage());
+        }
+    }
 }
--- app/src/main/java/com/pachalenlabs/wallp/module/WPLogger.java
+++ app/src/main/java/com/pachalenlabs/wallp/module/WPLogger.java
@@ -1,7 +1,42 @@
 package com.pachalenlabs.wallp.module;
 
+import android.content.Context;
+import android.os.Environment;
+
+import com.pachalenlabs.wallp.R;
+
+import org.apache.log4j.Level;
+
+import java.io.File;
+
+import de.mindpipe.android.logging.log4j.LogConfigurator;
+
 /**
  * Created by Niklane on 2016-01-12.
  */
 public class WPLogger {
+    public static void configure(Context ctx) {
+        LogConfigurator configurator = new LogConfigurator();
+
+        // path
+        String appName = ctx.getString(R.string.app_name);
+        String logPath = Environment.getExternalStorageDirectory() + File.separator + appName;
+
+        // create directory, which directory is not exists
+        new File(logPath).mkdirs();
+
+        logPath += File.separator + appName + ".log";
+
+        configurator.setFileName(logPath);
+        configurator.setFilePattern("%d - [%p::%C] - %m%n");     // log pattern
+        configurator.setMaxFileSize(1024 * 1024);                 // file size(byte)
+        configurator.setMaxBackupSize(10);                        // number of backup file
+
+        configurator.setRootLevel(Level.DEBUG);                  // set log level
+        configurator.setUseLogCatAppender(true);                 // and use Logcat
+
+        // set log level of a specific logger
+        configurator.setLevel("org.apache", Level.ERROR);
+        configurator.configure();
+    }
 }
