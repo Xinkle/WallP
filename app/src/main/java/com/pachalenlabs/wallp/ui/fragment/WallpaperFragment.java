487bfeee54d3ee949f886d4cb480239026878efd
KimTaeIk <tw2614@gmail.com>
2016-02-17 12:00:39 +0900

    WP-20  "Photo size exceeds solve"

--- app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
+++ app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
@@ -39,38 +39,24 @@ public class WallpaperFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //_wallPaperFragmentView = inflater.inflate(R.layout.wallpaper_fragment,container,false);
-        //_selectedPhotoImageView = (ImageView) _wallPaperFragmentView.findViewById(R.id.show_selected_photo_imageView);
-
-
-
-
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.wallpaper_fragment, container, false);
     }
 
     @AfterViews
     void setupViews(){
         BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
+        options.inSampleSize = 10;
         Bitmap bitmapImage = BitmapFactory.decodeResource(getResources(),R.drawable.test_wallpaper,options);
-        //Log.d("TAG","@@@@@@@@@@@@@@@@@"+String.valueOf(d));
         Bitmap resized = Bitmap.createScaledBitmap( bitmapImage,500,700, true );
         _selectedPhotoImageView.setImageBitmap(resized);
     }
 
     @Click(R.id.cancel_imageButton)
-    void leftViewClicked()
-    {
-        Log.d(Tag,"1");
-
-    }
+    void leftViewClicked() { Log.d(Tag,"1"); }
     @Click(R.id.right_imageButton)
     void cancelClicked(){
         Log.d(Tag,"2");
