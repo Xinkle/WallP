00411d4c2c1f5d6262504ab1398aa43a6d2adc92
Niklane <cswcsy@gmail.com>
2016-02-17 21:06:03 +0900

    Hotfix - Gradle Version Up, import Imageview

--- app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
+++ app/src/main/java/com/pachalenlabs/wallp/ui/fragment/WallpaperFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.pachalenlabs.wallp.R;
 
